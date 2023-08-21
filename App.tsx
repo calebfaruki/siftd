80cf96b26af7cdbd7baf02caa3daae43cd5b380d
Caleb Faruki <calebfaruki@hey.com>
2023-08-21 16:21:47 +0200

    fix: handle android status bar padding, app store assets

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, SafeAreaView, Platform, StatusBar } from 'react-native';
 import WebView from 'react-native-webview';
 
 export default function App() {
   return (
-    <WebView
-      style={styles.container}
-      source={{ uri: 'https://siftd.net/' }}
-    />
+    <SafeAreaView style={styles.container}>
+      <WebView
+        style={styles.webview}
+        source={{ uri: 'https://siftd.net/' }}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#171A1B',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+  webview: {
+    flex: 1,
   },
 });
